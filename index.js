1cb6cce923fd7b91d1b401257c8f66e6335b1912
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-22 08:47:27 -0500

    Not entirely sure why the NPM test is failing when this works just fine

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+const superBowlArray = [
+    {
+        year: 1993,
+        result: "L"
+    },
+    {
+        year: 1998,
+        result: "L"
+    },
+    {
+        year: 2015,
+        result: "W"
+    },
+    {
+        year: 1973,
+        result: "L"
+    },
+    {
+        year: 1956,
+        result: "L"
+    },
+    {
+        year: 2011,
+        result: "L"
+    }
+]
+superBowlArray.find(superbowlWin)
+
+function superbowlWin(element) {
+    if(element.result === "W") {
+        console.log(element.year)
+        return element.year.toString()
+    } 
+    return undefined
+}
